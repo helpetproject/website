04db1fac0d2ea1c68cafc8c4f4b06e8bc9ff522c
Juan Camilo Rios <zorencen@gmail.com>
2019-03-09 11:05:58 -0500

    Se crea la vista del equipo

---
+++ app/helpers/team_helper.rb
@@ -0,0 +1,11 @@
+module TeamHelper
+  def team_data
+    [
+      {name: 'Juan Ríos', avatar: '/assets/helpet_logo.png', role: 'CEO & Founder', linkedin: 'https://www.linkedin.com/in/juan-camilo-ríos/'},
+      {name: 'Laura Ríos', avatar: '/assets/helpet_logo.png', role: 'Community manager & Co-founder', linkedin: ''},
+      {name: 'Juan Garcia', avatar: '/assets/helpet_logo.png', role: 'Project manager', linkedin: 'https://www.linkedin.com/in/juan-diego-garcia-mejia-a163b03b'},
+      {name: 'Paulo Carmona', avatar: '/assets/helpet_logo.png', role: 'RoR architect', linkedin: 'https://www.linkedin.com/in/pcarmonaz/'},
+      {name: 'Ricardo Alarcón', avatar: '/assets/helpet_logo.png', role: 'RoR & Mobile developer', linkedin: 'https://www.linkedin.com/in/ricardo-alarcon-44079b105/'},
+    ]
+  end
+end
