1c1b567b643f8dbcf8cb8f912e92e7c0df25f46b
Zorencen R <zorencen@gmail.com>
2019-06-16 10:21:19 -0500

    reestructuracion sitio web

--- app/helpers/team_helper.rb
+++ app/helpers/team_helper.rb
@@ -3,9 +3,7 @@ module TeamHelper
     [
       {name: 'Juan Ríos', avatar: '/assets/team/juanrios.jpeg', role: 'CEO & Founder', linkedin: 'https://www.linkedin.com/in/juan-camilo-ríos/'},
       {name: 'Laura Ríos', avatar: '/assets/team/sofia.jpg', role: 'Community manager & Co-founder', linkedin: ''},
-      {name: 'Juan Garcia', avatar: '/assets/team/juandiego.jpg', role: 'Project manager', linkedin: 'https://www.linkedin.com/in/juan-diego-garcia-mejia-a163b03b'},
-      {name: 'Paulo Carmona', avatar: '/assets/team/paulo.jpg', role: 'RoR architect', linkedin: 'https://www.linkedin.com/in/pcarmonaz/'},
-      {name: 'Ricardo Alarcón', avatar: '/assets/helpet_logo.png', role: 'RoR & Mobile developer', linkedin: 'https://www.linkedin.com/in/ricardo-alarcon-44079b105/'},
+      {name: 'Juan Garcia', avatar: '/assets/team/juandiego.jpg', role: '', linkedin: 'https://www.linkedin.com/in/juan-diego-garcia-mejia-a163b03b'},
     ]
   end
 end
